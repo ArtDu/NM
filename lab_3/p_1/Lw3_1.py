adbce83447f81717e2ed01a622b518ce5b8ce58d
ArtDub <rusartdub@gmail.com>
2020-05-10 17:16:22 +0300

    lab3 p1 fix

--- lab_3/p_1/Lw3_1.py
+++ lab_3/p_1/Lw3_1.py
@@ -6,10 +6,20 @@ def lagrange(points):
     def inner(x):
         cnt_points = len(points)
         l = []
+        omega_apostrophe = []
         for i in range(cnt_points):
             l.append(np.prod([(x - points[j][0]) / (points[i][0] - points[j][0])
                               for j in range(cnt_points) if i != j]))
-        return sum([points[idx][1] * i for idx, i in enumerate(l)])
+            omega_apostrophe.append(round(np.prod([(points[i][0] - points[j][0])
+                                                   for j in range(cnt_points) if i != j]), 3))
+        result = sum([points[idx][1] * i for idx, i in enumerate(l)])
+        f_div_omega_ap = [round(points[idx][1] / i, 5) for idx, i in enumerate(omega_apostrophe)]
+        x_minus_xi = [round(x - xi_fi[0], 1) for xi_fi in points]
+        table_i = [i for i in range(cnt_points)]
+        table_xi = [xi_fi[0] for xi_fi in points]
+        table_fi = [round(xi_fi[1], 5) for xi_fi in points]
+        return result, table_i, table_xi, table_fi, omega_apostrophe, f_div_omega_ap, x_minus_xi
+
     return inner
 
 
@@ -30,15 +40,22 @@ def newton(points):
         res = 0
         for i in range(cnt_points):
             res += coef[i] * np.prod([x - x_point[j] for j in range(i)])
-        return res
+        P = f'{coef[0]} + {round(coef[1],5)}(x - {x_point[0]}) + ' \
+            f'{round(coef[2],5)}(x - {x_point[0]})(x - {x_point[1]}) + ' \
+            f'{round(coef[3],5)}(x - {x_point[0]})(x - {x_point[1]})(x - {x_point[2]}).\n'
+
+        return res, P
+
     return inner
 
 
 def get_points(file_name):
     with open(file_name) as f:
         x = [float(num) * np.pi for num in f.readline().split()]
-        points = [(i, np.sin(i)) for i in x]
+        # x = [float(num) for num in f.readline().split()]
+        points = [(round(i,5), round(np.sin(i), 5)) for i in x]
         x_test = float(f.readline()) * np.pi
+        # x_test = float(f.readline())
     return points, x_test
 
 
@@ -54,14 +71,27 @@ def main():
 
     with open(args.output, 'w') as f:
         tmp = round(x_test, 3)
-        res1 = lg(tmp)
-        res2 = nt(tmp)
-        res3 = np.sin(tmp)
-        eps1 = abs(res1 - res3)
-        eps2 = abs(res2 - res3)
-        f.write(f'Lagrange:\nL({tmp}) = {res1}\nEps = {round(eps1, 5)}\n')
-        f.write(f'Newton:\nN({tmp}) = {res2}\nEps = {round(eps2, 5)}\n')
-        f.write(f'Orig:\nSin({tmp}) = {res3}\n')
+        res1, table_i, table_xi, table_fi, omega_apostrophe, f_div_omega_ap, x_minus_xi = lg(tmp)
+        true_val = np.sin(tmp)
+        # true_val = np.log(tmp)
+        eps1 = abs(res1 - true_val)
+        f.write(f'Lagrange:\n L({tmp}) = {res1}\n')
+        f.write(f' y({tmp}): {round(true_val, 5)}\n')
+        f.write(f' Eps = {round(eps1, 5)}\n')
+        f.write(f' i: {table_i}\n')
+        f.write(f' xi: {table_xi}\n')
+        f.write(f' fi: {table_fi}\n')
+        f.write(f' omega\'(xi): {omega_apostrophe}\n')
+        f.write(f' fi/omega\'(xi): {f_div_omega_ap}\n')
+        f.write(f' X* - xi: {x_minus_xi}\n')
+
+        res2, P = nt(tmp)
+        eps2 = abs(res2 - true_val)
+        f.write(f'Newton:\n N({tmp}) = {res2}\n ')
+        f.write(P)
+        f.write(f' Eps = {round(eps2, 5)}\n')
+        f.write(f'Orig:\n Sin({tmp}) = {true_val}\n')
+
 
 if __name__ == "__main__":
     main()
